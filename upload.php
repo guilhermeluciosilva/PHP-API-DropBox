dc56b5b007f5d84d0981f6a1ad6da50f6e1c19c6
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-07-22 13:28:30 -0300

    atualizando versao

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'terceros/dropbox/vendor/autoload.php';
+use Kunnu\Dropbox\Dropbox;
+use Kunnu\Dropbox\DropboxApp;
+
+$dropboxKey = "keyexemplo";
+$dropboxSecret = "Secretexemplo
+";
+$dropboxToken = "Tokenexemplo";
+
+$app = new DropboxApp($dropboxKey, $dropboxSecret, $dropboxToken);
+$dropbox = new Dropbox($app);
+
+if(!empty($_FILES)){
+    $nombre = uniqid();
+    $temfile = $_FILES['file']['tmp_name'];
+    $ext = explode(".",$_FILES['file']['name']);
+    $ext = end($ext);
+    $nombredropbox = "/".$nombre.".".$ext;
+
+    try{
+        $file = $dropbox->simpleUpload($temfile, $nombredropbox , ['autorename' => true]);
+        echo "arquivo subido";
+    } catch(\exception $e) {
+            print_r($e);
+    }
+
+}
+
+?>
